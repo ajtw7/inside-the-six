7454cca96c4c4e6fa229782e6466e7b101ec634c
Andrew <ayjtw7@gmail.com>
2022-09-20 17:52:32 -0400

    connecting leagues to teams and teams to players

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -34,6 +34,43 @@ router.get('/leagues', (req, res, next) => {
   // res.render('leagues')
 });
 
+// Get Teams from a specific league 
+router.get('/leagues/:leagueCode/teams', (req, res, next) => {
+  const leagueCode = req.params.leagueCode
+  axios({
+      method: 'get',
+      url: `https://api.football-data.org/v4/competitions/${leagueCode}/teams`,
+      headers: {
+        "X-Auth-Token": "bd8fb1ba95854895b437b0e5ffc4bf1a"
+      }
+    })
+    .then(axiosResponse => {
+      console.log(axiosResponse.data.teams[0])
+      res.render('league-teams', {
+        leagueTeam: axiosResponse.data.teams
+      })
+    })
+    .catch(err => console.log(err))
+})
+
+// Get entire roster from a specific team
+router.get('/teams/:teamCode/roster', (req, res, next) => {
+  const teamCode = req.params.teamCode
+  axios({
+      method: 'get',
+      url: `https://api.football-data.org/v4/teams/${teamCode}`,
+      headers: {
+        "X-Auth-Token": "bd8fb1ba95854895b437b0e5ffc4bf1a"
+      }
+    })
+    .then(axiosResponse => {
+      console.log(axiosResponse.data)
+      res.render('roster', {
+        team: axiosResponse.data
+      })
+    })
+    .catch(err => console.log(err))
+})
 
 // Get  All Teams Page
 router.get('/all-teams', (req, res, next) => {
@@ -52,27 +89,5 @@ router.get('/all-teams', (req, res, next) => {
     })
     .catch(err => console.log(err))
 })
-
-// Get Players Page
-
-router.get('/leagues/teams', (req, res, next) => {
-  axios({
-      method: 'get',
-      url: "https://api.football-data.org/v4/competitions/PL/teams",
-      headers: {
-        "X-Auth-Token": "bd8fb1ba95854895b437b0e5ffc4bf1a"
-      }
-    })
-    .then(axiosResponse => {
-      console.log(axiosResponse.data.teams[0])
-      res.render('league-teams', {
-        leagueTeam: axiosResponse.data.teams
-      })
-    })
-    .catch(err => console.log(err))
-
-
-})
-
 
 module.exports = router;

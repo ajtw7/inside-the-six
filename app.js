3c181d5caa3c7e4cf6fcf316ab4420c23d965034
Andrew <ayjtw7@gmail.com>
2022-09-22 00:34:29 -0400

    adding authentication to signup page

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const index = require("./routes/index.routes");
 app.use("/", index);
 
 const authRouter = require("./routes/auth.routes");
-app.use("/auth", authRouter)
+app.use("/", authRouter)
 
 const watchlistRouter = require("./routes/watchlist.routes")
 app.use("/watchlist", watchlistRouter)
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,6 +5,7 @@ const router = new Router();
 const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
 const User = require('../models/User.model')
+const mongoose = require('mongoose');
 
 
 
@@ -22,6 +23,13 @@ router.post('/signup', (req, res, next) => {
         password
     } = req.body;
 
+    if (!username || !email || !password) {
+        res.render('auth/signup', {
+            errorMessage: 'All fields are mandatory. Please provide your username, email and password.'
+        })
+        return
+    }
+
     bcryptjs
         .genSalt(saltRounds)
         .then(salt => bcryptjs.hash(password, salt))
@@ -37,7 +45,21 @@ router.post('/signup', (req, res, next) => {
             console.log('The newest user is: ', userFromDB);
             res.redirect('/userProfile')
         })
-        .catch(error => next(error));
+        .catch(error => {
+            if (error instanceof mongoose.Error.ValidationError) {
+                res.status(500).render('auth/signup', {
+                    errorMessage: error.message
+                });
+            } else if (error.code === 11000) {
+                res.status(500).render('auth/signup', {
+                    errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+                })
+            } else {
+                next(error)
+            }
+
+
+        });
 });
 
 router.get('/userProfile', (req, res, next) => {
